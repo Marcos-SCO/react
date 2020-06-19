d0bbe28dddb90f1479d0f8ce83d5be7025b767de
Marcos-SCO <marcos_sco@outlook.com>
2020-06-19 15:00:01 -0300

    Result not found bug fixed

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,9 +5,9 @@ import Result from './Result'
 function Results ({ results, openPopup }) {
 	return (
 		<section className="results">
-			{results.map(result => (
+			{ (results != undefined) ? results.map(result => (
 				<Result key={result.imdbID} result={result} openPopup={openPopup} />
-			))}
+			)) : <header className="content"><h1>Not found</h1></header> }
 		</section>
 	)
 }
