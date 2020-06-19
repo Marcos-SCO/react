0abd8568a0ab2db42debed0f480a85c565604667
Marcos-SCO <marcos_sco@outlook.com>
2020-06-19 14:41:15 -0300

    On error popup bug fix

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,10 +4,12 @@ function Popup({ selected, closePopup }) {
 	return (
 		<section className="popup">
 			<div className="content">
-				<h2>{ selected.Title } <span>({ selected.Year })</span></h2>
+				<h2>{selected.Title} <span>({selected.Year})</span></h2>
 				<p className="rating">Rating: {selected.imdbRating}</p>
 				<div className="plot">
-					<img src={selected.Poster} />
+					<img src={selected.Poster} onError={(e) => {
+						e.target.src = 'https://raw.githubusercontent.com/Marcos-SCO/gordao110porcento/master/public/img/not_found/no_image.jpg'
+					}} />
 					<p>{selected.Plot}</p>
 				</div>
 				<button className="close" onClick={closePopup}>Close</button>
