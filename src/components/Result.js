e3d4c132efdc35095ceaf357a30cefe7cfe733e3
Marcos-SCO <marcos_sco@outlook.com>
2020-06-19 14:36:35 -0300

    On error img fix

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,7 +3,9 @@ import React from 'react'
 function Result({ result, openPopup }) {
 	return (
 		<div className="result" onClick={() => openPopup(result.imdbID)}>
-			<img src={result.Poster} alt={result.title} title={result.Title} />
+			<img src={result.Poster} alt={result.title} title={result.Title} onError={(e) => {
+				e.target.src = 'https://raw.githubusercontent.com/Marcos-SCO/gordao110porcento/master/public/img/not_found/no_image.jpg'
+			}} />
 			<h3>{result.Title}</h3>
 		</div>
 	)
