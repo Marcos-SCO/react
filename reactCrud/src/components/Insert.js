077089d304ddfbe67f06637778a19647b66f4908
Marcos-SCO <marcos_sco@outlook.com>
2020-12-16 23:17:17 -0300

    insert form

--- reactCrud/src/components/Insert.js
+++ reactCrud/src/components/Insert.js
@@ -4,7 +4,25 @@ export default class Insert extends Component {
     render() {
         return (
             <div>
-                <p>Welcome insert</p>
+                <div style={{ marginTop: 10 }}>
+                    <h3>Add new user</h3>
+                    <form>
+                        <div className="form-group">
+                            <label>First name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label>Last name</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input type="text" className="form-control" />
+                        </div>
+
+                        <input type="submit" value="Register user" className="btn btn-primary" />
+                    </form>
+                </div>
             </div>
         )
     }
