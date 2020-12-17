81386da886bd8a07fb01f5826dae0f88b452b0c7
Marcos-SCO <marcos_sco@outlook.com>
2020-12-16 23:39:54 -0300

    insert form props and state

--- reactCrud/src/components/Insert.js
+++ reactCrud/src/components/Insert.js
@@ -1,23 +1,79 @@
 import React, { Component } from 'react'
 
 export default class Insert extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onChangeFirstName = this.onChangeFirstName.bind(this);
+        this.onChangeLastName = this.onChangeLastName.bind(this);
+        this.onChangeEmail = this.onChangeEmail.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            first_name: '',
+            last_name: '',
+            email: '',
+        }
+    }
+
+    onChangeFirstName(e) {
+        this.setState({
+            first_name: e.target.value
+        });
+    }
+
+    onChangeLastName(e) {
+        this.setState({
+            last_name: e.target.value
+        });
+    }
+
+    onChangeEmail(e) {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const obj = {
+            fist_name: this.state.first_name,
+            last_name: this.state.last_name,
+            email: this.state.email,
+        };
+
+        console.log(obj);
+    }
+
     render() {
         return (
             <div>
                 <div style={{ marginTop: 10 }}>
                     <h3>Add new user</h3>
-                    <form>
+                    <form onSubmit={this.onSubmit}>
                         <div className="form-group">
                             <label>First name</label>
-                            <input type="text" className="form-control" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={this.state.first_name} onChange={this.onChangeFirstName} />
                         </div>
                         <div className="form-group">
                             <label>Last name</label>
-                            <input type="text" className="form-control" />
+                            <input
+                                type="text"
+                                className="form-control"
+                                value={this.state.last_name}
+                                onChange={this.onChangeLastName} />
                         </div>
                         <div className="form-group">
                             <label>Email</label>
-                            <input type="text" className="form-control" />
+                            <input
+                                type="text" className="form-control"
+                                value={this.state.email}
+                                onChange={this.onChangeEmail} />
                         </div>
 
                         <input type="submit" value="Register user" className="btn btn-primary" />
