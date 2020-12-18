245b7d28a13d2f7064ce34dfb124e091220b6e32
Marcos-SCO <marcos_sco@outlook.com>
2020-12-17 22:18:45 -0300

    Css custom classes

--- 0_cart_begginners/src/components/Header.js
+++ 0_cart_begginners/src/components/Header.js
@@ -3,8 +3,16 @@ import React from 'react';
 
 export default function Header(props) {
     return (
-        <div>
-            header
-        </div>
+        <header class="row block center">
+            <div>
+                <a href="/">
+                    <h1>Small Shopping Cart</h1>
+                </a>
+            </div>
+            <div>
+                <a href="#/cart">Cart</a>
+                <a href="#/siging">SigIn</a>
+            </div>
+        </header>
     );
 };
