875cc0d45ce850a87a7436bf27bae04020fb5680
Marcos-SCO <marcos_sco@outlook.com>
2020-12-17 22:30:13 -0300

    Data for cart shop

--- 0_cart_begginners/src/App.js
+++ 0_cart_begginners/src/App.js
@@ -3,14 +3,16 @@ import Basket from './components/Basket';
 import Header from './components/Header';
 import Main from './components/Main';
 
+import data from './data';
+
 function App() {
   return (
     <div className="App">
       <Header />
-      <>
+      <div className="row">
         <Main/>
         <Basket/>
-      </>
+      </div>
     </div>
   );
 }
